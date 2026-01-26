e09b95ab6e3e6e94ca60a36f97e3ded5f3310ccd
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-01-26 14:25:11 +0545

    fixed:profile card

--- github-profile-analyzer/src/components/ProfileCard.jsx
+++ github-profile-analyzer/src/components/ProfileCard.jsx
@@ -1,28 +1,33 @@
 import { useGithubProfile } from "../hooks/useGithubProfile";
+import { calculateDeveloperScore } from "../utils/scoreCalculator";
+import ScoreBadge from "./ScoreBadge";
+
 import StatCard from "./StatCard";
 export default function ProfileCard({ username }) {
   const { user, repos, loading, error } = useGithubProfile(username);
+
+  const score = calculateDeveloperScore(user, repos);
 
   if (loading) return <p>Loading profile...</p>;
   if (error) return <p>{error}</p>;
   if (!user) return null;
 
   return (
-    <div className="profile-card">
-      <img src={user.avatar_url} alt="avatar" />
-      <div>
-        <div className="stats-grid">
-  <StatCard label="Followers" value={user.followers} />
-  <StatCard label="Public Repos" value={user.public_repos} />
-  <StatCard label="Following" value={user.following} />
-</div>
-        <h2>{user.name || user.login}</h2>
-        <p>{user.bio}</p>
-        <div className="stats">
-          <span>👥 {user.followers} Followers</span>
-          <span>📦 {repos.length} Repos</span>
-        </div>
-      </div>
+   <div className="profile-card">
+  <img src={user.avatar_url} alt="avatar" />
+
+  <div style={{ flex: 1 }}>
+    <h2>{user.name || user.login}</h2>
+    <p>{user.bio}</p>
+
+    <div className="stats">
+      <span>👥 {user.followers} Followers</span>
+      <span>📦 {repos.length} Repos</span>
     </div>
+  </div>
+
+  <ScoreBadge score={score} />
+</div>
+
   );
 }
