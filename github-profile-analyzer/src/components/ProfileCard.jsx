22cf68e5b8af31e74c47eb408c9f0c6b24634a41
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-01-26 13:56:05 +0545

    Changed:profilecard

--- github-profile-analyzer/src/components/ProfileCard.jsx
+++ github-profile-analyzer/src/components/ProfileCard.jsx
@@ -3,17 +3,21 @@ import { useGithubProfile } from "../hooks/useGithubProfile";
 export default function ProfileCard({ username }) {
   const { user, repos, loading, error } = useGithubProfile(username);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <p>Loading profile...</p>;
   if (error) return <p>{error}</p>;
   if (!user) return null;
 
   return (
-    <div>
-      <img src={user.avatar_url} width="100" />
-      <h2>{user.name}</h2>
-      <p>{user.bio}</p>
-      <p>Followers: {user.followers}</p>
-      <p>Public Repos: {user.public_repos}</p>
+    <div className="profile-card">
+      <img src={user.avatar_url} alt="avatar" />
+      <div>
+        <h2>{user.name || user.login}</h2>
+        <p>{user.bio}</p>
+        <div className="stats">
+          <span>👥 {user.followers} Followers</span>
+          <span>📦 {repos.length} Repos</span>
+        </div>
+      </div>
     </div>
   );
 }
