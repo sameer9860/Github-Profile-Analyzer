9d481778e826e76d64182bf9c49430b77cc8affb
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-23 21:25:13 +0545

    Fixed with star and fork

--- github-profile-analyzer/src/utils/repoUtils.js
+++ github-profile-analyzer/src/utils/repoUtils.js
@@ -1,5 +1,10 @@
 export function getTopRepositories(repos, limit = 5) {
   return [...repos]
-    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .sort(
+      (a, b) =>
+        b.stargazers_count +
+        b.forks_count -
+        (a.stargazers_count + a.forks_count)
+    )
     .slice(0, limit);
 }
